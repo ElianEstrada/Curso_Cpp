3709fcce2937956159858f6d0204200851fea6de
ElianEstrada <elian.estrada.urbina@gmail.com>
2022-10-25 21:42:49 -0600

    Moviendo el archivo de la clase 3 a una carpeta para manejar los ejemplos

---
+++ clase3/clase3_25oct22_ej2.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /*Tipos de Operadores
+
+    Operadores Aritmeticos
+    ---------los operadores mas comunes---------
+    + -> sirve para sumar
+    - -> sirve para restar
+    * -> sirve para multiplicar
+    / -> sirve para dividir
+    // -> sirve para divisiones enteras
+
+    ---------operadores especiales----------
+    % -> modulo -> sirve para obtener el residuo de una división
+    2/2 -> 1 -> cociente -> 0 mi residuo
+    3/2 -> 0 -> cociente -> 2 mi residuo
+
+    ++ -> incremento -> sirve para aumentar en 1 la variable (ojo que solo funciona con varaibles de tipo int y float)
+    -- -> decremento -> siver para disminuir en 1 la variable (ojo que solo funciona con varaibles de tipo int y float)
+
+    Operadores Relacionales
+
+    > -> sirve para saber si el valor de la izquierda es mayor al de la derecha (17>17) -> falso
+    >= -> siver para saber si el valor de la izquierda es mayor o igual al de la derecha -> (17>=17) -> verdadero
+    <   -> sirve para saber si el valor de la izquierda es menor al de la derecha
+    <=  -> sirve para saber si el valor de la izquierda es menor o igual al de la derecha
+
+    == -> igualacion -> sirve para saber si el valor de la izquierda es igual al de la derecha (12 == 12) -> verdadero
+    != -> diferente -> sireve para saber si el valor de la izquierda es diferente al de la derecha (12 != 1) -> verdadero
+
+    Operadores Logicos
+    && -> and
+    | true | true | true |
+    | true | false | false |
+    | false | true | false |
+    | false | false | false |
+
+    true && true -> verdadero 
+    12 > 1 && 12 < 10 -> 
+     true  &&    false -> false
+
+    || -> or
+    | true | true | true |
+    | true | false | true |
+    | false | true | true |
+    | false | false | false |
+
+    true || false -> verdadero
+
+    12 < 10 || 12 > 5
+     false  ||   true -> verdadero
+
+    ! -> negacion
+
+    |true | false|
+    |false | true| 
+
+    !true -> false
+    !false -> true
+
+    */
+
+
+   /*
+        Solicitar al usuario un número entero,
+        y mostrar si ese número es par o impar.
+   */
+
+}
